f822e8f43e9860ba482a20de5af036d49e3e74f2
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-03-15 01:03:44 +0100

    start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./Styles.scss";
+
+const App = () => {
+  return ( <div className="calculator-grid">
+    <div className="output">
+      
+    </div>
+  </div> );
+}
+ 
+export default App;
